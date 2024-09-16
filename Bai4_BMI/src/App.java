7eb0094c1705d98c4dde1151dde6b684d1c0d1ff
QuHung-0 <hung.nlq.64cntt@ntu.edu.vn>
2024-09-16 13:07:06 +0700

    Update App.java

--- Bai4_BMI/src/App.java
+++ Bai4_BMI/src/App.java
@@ -2,44 +2,78 @@ import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner banphim = new Scanner(System.in);
-		
-		float canNang, chieuCao, BMI;
-		
-		System.out.print("Tinh BMI\n");
-		
-		System.out.print("Nhap can nang (kg): ");
-		canNang = banphim.nextFloat();
+        Scanner banphim = new Scanner(System.in);
 
-		
-		System.out.print("Nhap chieu cao (m): ");
-		chieuCao = banphim.nextFloat();
-		
-		BMI = (canNang / (chieuCao * chieuCao));
-		
-		if(BMI < 18.5)
-		{
-			System.out.print("BMI: " + BMI + ", Thieu can");
-		}
-		else if (18.5 < BMI && BMI < 24.9)
-		{
-			System.out.print("BMI: " + BMI + ", binh thuong");
-		}
-		else if (24.9 < BMI && BMI < 30)
-		{
-			System.out.print("BMI: " + BMI + ", thua can");
-		}
-		else if (BMI > 30)
-		{
-			System.out.print("BMI: " + BMI + ", beo phi");
-		}
-		else
-		{
-			System.out.print(BMI + " out of bound");
-		}
-		
-	}
-	
+        float canNang = 0, chieuCao = 0, BMI;
+
+        System.out.print("Tinh BMI\n");
+
+        while (true)
+        {
+            try
+            {
+                System.out.print("Nhap can nang (kg): ");
+                canNang = banphim.nextFloat();
+
+                if (canNang > 0)
+                {
+                    break;
+                }
+                else if (canNang < 0)
+                {
+                    System.out.println("Can nang phai lon hon 0, hay nhap lai");
+                    canNang = banphim.nextFloat();
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Gia tri nhap khong hop le");
+                banphim.next();
+            }
+
+        }
+
+        while (true)
+        {
+            try
+            {
+                System.out.print("Nhap chieu cao (m): ");
+                chieuCao = banphim.nextFloat();
+
+                if (chieuCao  > 0)
+                {
+                    break;
+                }
+                else if (chieuCao < 0)
+                {
+                    System.out.println("chieu cao phai lon hon 0, hay nhap lai");
+                    chieuCao = banphim.nextFloat();
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println("Gia tri nhap khong hop le");
+                banphim.next();
+            }
+
+        }
+
+        BMI = (canNang / (chieuCao * chieuCao));
+
+        if (BMI < 18.5)
+        {
+            System.out.print("BMI: " + BMI + ", Thieu can");
+        } else if (18.5 <= BMI && BMI < 24.9)
+        {
+            System.out.print("BMI: " + BMI + ", Binh thuong");
+        } else if (24.9 <= BMI && BMI < 30)
+        {
+            System.out.print("BMI: " + BMI + ", Thua can");
+        } else if (BMI >= 30)
+        {
+            System.out.print("BMI: " + BMI + ", Beo phi");
+        }
+    }
 }
