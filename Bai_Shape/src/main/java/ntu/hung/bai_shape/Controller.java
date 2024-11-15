2bf9249717c5365dbc13b070079009b794bfcd00
QuHung-0 <hung.nlq.64cntt@ntu.edu.vn>
2024-11-15 15:27:19 +0700

    Update Controller.java

--- Bai_Shape/src/main/java/ntu/hung/bai_shape/Controller.java
+++ Bai_Shape/src/main/java/ntu/hung/bai_shape/Controller.java
@@ -5,32 +5,47 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class Controller {
+public class Controller
+{
 
     @FXML
     private Canvas canvas;
 
-    public void initialize() {
+    public void initialize()
+    {
         drawShape();
     }
 
-    private void drawShape() {
+    private void drawShape()
+    {
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        //Hinh Tron
         gc.setStroke(Color.BLACK);
+        gc.setLineWidth(3);
+        gc.strokeOval(100, 180, 100, 100);
+
+        //Tam giac To
+        gc.setStroke(Color.BLACK);
+        gc.setFill(Color.WHITE);
         double[] xPoints = {150, 50, 250};
         double[] yPoints = {50, 200, 200};
         gc.strokePolygon(xPoints, yPoints, 3);
 
+
         gc.setStroke(Color.RED);
         double[] xInnerPoints = {150, 130, 170};
-        double[] yInnerPoints = {120, 160, 160};
+        double[] yInnerPoints = {160, 120, 120};
         gc.strokePolygon(xInnerPoints, yInnerPoints, 3);
 
-        
+        gc.setStroke(Color.BLACK);
+        double[] xHorn1 = {115, 115, 130};
+        double[] yHorn1 = {100, 50, 75};
+        double[] xHorn2 = {185, 185, 170};
+        double[] yHorn2 = {100, 50, 75};
+        gc.strokePolygon(xHorn1, yHorn1, 3);
+        gc.strokePolygon(xHorn2, yHorn2, 3);
 
-
-        gc.strokeOval(100, 200, 100, 100);
 
 
         gc.strokeOval(110, 280, 30, 30);
